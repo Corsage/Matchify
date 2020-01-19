3d61c850fea145e7cc19319177ed4c56bd428f46
Jay C <jayc.chowdhary@gmail.com>
2020-01-19 02:38:39 -0500

    Commonly used api constants.

---
+++ app/src/main/java/com/corsage/memory_matching/util/ApiConstants.kt
@@ -0,0 +1,16 @@
+package com.corsage.memory_matching.util
+
+/**
+ * @author [Jay Chowdhary](https://github.com/Corsage)
+ * @since 1.0
+ *
+ * API Constants.
+ * Contains commonly used API constants.
+ */
+
+class ApiConstants {
+    companion object {
+        const val API_URL = "https://shopicruit.myshopify.com/admin/"
+        const val API_ACCESS_TOKEN = "access_token"
+    }
+}
